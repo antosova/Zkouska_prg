4fafead9e55ff858f9698dc0c7182491a2617763
Kristýna Antošová <antosovkr@natur.cuni.cz>
2023-02-24 16:02:13 +0000

    ctvrty commit

--- insert_sort.py
+++ insert_sort.py
@@ -1,15 +1,55 @@
-array = [2, 8, 4, 12, 6, 1, 10]
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        j = i
-        while arr[j] < arr[j-1] and j != 0:
-            arr[j], arr[j-1] = arr[j-1], arr[j]
-            j -= 1
-    return arr
+import file_handler as fh
 
-print('unsorted: ' + str(array))
+class Sorter:
+    def __init__(self):
+        correct_input = False
+        while not correct_input:
+            print('Sort in ascending (a) or descending (d) order?')
+            order = input()
+            if order == 'a':
+                self.ascending = True
+                correct_input = True
+            elif order == 'd':
+                self.ascending = False
+                correct_input = True
+            else:
+                print('Incorrect input')
+            self.done = False
+    
+    def set_array_to_sort(self, array):
+        self.unsorted = array
+        self.sorted = array
+        self.done = False
 
-sorted = insertion_sort(array)
+    def sort_using_insertion_sort(self):
+        for i in range(1, len(self.unsorted)):
+            j = i
+            if self.ascending:
+                while self.sorted[j] < self.sorted[j-1] and j != 0:
+                    self.sorted[j], self.sorted[j-1] = self.sorted[j-1], self.sorted[j]
+                    j -= 1
+            else:
+                while self.sorted[j] > self.sorted[j-1] and j != 0:
+                    self.sorted[j], self.sorted[j-1] = self.sorted[j-1], self.sorted[j]
+                    j -= 1
+        self.done = True
+    
+    def print_sorted_array(self):
+        if self.done == False:
+            print('Array hasn\'t been sorted yet.')
+        else:
+            print(str(self.sorted))
 
-print('sorted: ' + str(sorted))
+input_file_name = 'numbers.txt'
+output_file_name = 'sorted.txt'
+
+f_handler = fh.FileHandler()
+array = f_handler.load_as_numbers_list(input_file_name)
+
+sorter = Sorter()
+sorter.set_array_to_sort(array)
+sorter.sort_using_insertion_sort()
+sorter.print_sorted_array()
+
+f_handler.write_array_to_file(output_file_name, sorter.sorted)
