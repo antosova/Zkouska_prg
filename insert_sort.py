0267ad7565f460373189aa83ee2c0e374e2db03e
Kristýna Antošová <antosovkr@natur.cuni.cz>
2023-02-24 18:35:27 +0000

    comments

--- insert_sort.py
+++ insert_sort.py
@@ -2,6 +2,7 @@
 import file_handler as fh
 
 class Sorter:
+    #asks a user if he/she wants to sort in descending or ascending order
     def __init__(self):
         correct_input = False
         while not correct_input:
@@ -16,12 +17,13 @@ class Sorter:
             else:
                 print('Incorrect input')
             self.done = False
-    
+    # creates sorted and unsorted arrays
     def set_array_to_sort(self, array):
         self.unsorted = array
         self.sorted = array
         self.done = False
 
+    # algorithm for insertion sort
     def sort_using_insertion_sort(self):
         for i in range(1, len(self.unsorted)):
             j = i
@@ -34,13 +36,14 @@ class Sorter:
                     self.sorted[j], self.sorted[j-1] = self.sorted[j-1], self.sorted[j]
                     j -= 1
         self.done = True
-    
+    # if sorting has been completed, prints the sorted numbers
     def print_sorted_array(self):
         if self.done == False:
             print('Array hasn\'t been sorted yet.')
         else:
             print(str(self.sorted))
 
+# loads data from the input file and makes list of numbers
 input_file_name = 'numbers.txt'
 output_file_name = 'sorted.txt'
 
@@ -52,4 +55,5 @@ sorter.set_array_to_sort(array)
 sorter.sort_using_insertion_sort()
 sorter.print_sorted_array()
 
+# makes output text file 
 f_handler.write_array_to_file(output_file_name, sorter.sorted)
